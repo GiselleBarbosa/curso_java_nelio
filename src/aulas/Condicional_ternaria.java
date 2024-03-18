3e257e1202f7261ef89995611b2e80e427dc9416
GiselleBarbosa <g88barbosa@gmail.com>
2024-03-18 13:00:11 -0300

    adicionado exemplo com ternarios

---
+++ src/aulas/Condicional_ternaria.java
@@ -0,0 +1,33 @@
+package aulas;
+
+import java.util.Scanner;
+
+public class Condicional_ternaria {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		 double preco = scanner.nextDouble(); 
+
+		/*
+		 * double desconto;
+		 * 
+		 * if (preco > 100.0) { 
+		 * 		desconto = preco * 0.1; 
+		 * } 
+		 * 
+		 * else { 
+		 * 		desconto = preco *
+		 * 0.05; 
+		 * } 
+		 */
+
+		// Utilizando ternarios
+
+		double desconto = (preco < 20.0 ? preco * 0.1 : preco * 0.05);
+		System.out.println(desconto);
+
+		scanner.close();
+	}
+
+}
