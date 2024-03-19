2e34326dcae8ab6e0ed752361f339391e1ae7bd7
GiselleBarbosa <g88barbosa@gmail.com>
2024-03-19 17:09:59 -0300

    adicionado novo exemplo - criada classe Products

---
+++ src/aulas/j05_orientacao_objeto/application/Program.java
@@ -0,0 +1,28 @@
+package aulas.j05_orientacao_objeto.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import aulas.j05_orientacao_objeto.entitys.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+
+		Product product = new Product();
+
+		System.out.println("Enter product data: ");
+		System.out.print("Name: ");
+		product.name = scanner.next();
+		System.out.print("Price: ");
+		product.price = scanner.nextDouble();
+		System.out.print("Quantity: ");
+		product.quantity = scanner.nextInt();
+
+		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		scanner.close();
+	}
+
+}
---
+++ src/aulas/j05_orientacao_objeto/entitys/Product.java
@@ -0,0 +1,20 @@
+package aulas.j05_orientacao_objeto.entitys;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quantity;
+
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+
+	}
+}
