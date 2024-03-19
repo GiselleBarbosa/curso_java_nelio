821c0d8c0cdc8f6e8005514047c97ea707a07a22
GiselleBarbosa <g88barbosa@gmail.com>
2024-03-18 22:00:23 -0300

    adicionado exemplo de operadores Bitwise

---
+++ src/outros_exemplos/Operadores_bitwise.java
@@ -0,0 +1,14 @@
+package outros_exemplos;
+
+public class Operadores_bitwise {
+
+	public static void main(String[] args) {
+		int numero1 = 89;
+		int numero2 = 60;
+
+		System.out.println(numero1 & numero2);
+		System.out.println(numero1 | numero2);
+		System.out.println(numero1 ^ numero2);
+	}
+
+}
